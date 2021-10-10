884927b4c16fb0c713f3fb81b4b307d05eee8976
anton <nba15melo@gmail.com>
2021-10-11 00:46:23 +0300

    added fetch placeholder

--- src/components/MainContent/ContentArea.js
+++ src/components/MainContent/ContentArea.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./ContentArea.css";
 
 function ContentArea() {
-  return <div className="area"></div>;
+  const [data, setData] = useState([]);
+
+  const getApi = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+  return (
+    <div className="area">
+      <p>{JSON.stringify({ data })}</p>
+    </div>
+  );
 }
 
 export default ContentArea;
